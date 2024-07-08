86936b25893296e4c2f03912a62cda726dabea97
Ilya Tumakov <ilyanikt2011@mail.ru>
2024-07-08 08:06:34 +0400

    Initial Commit

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Ilya Tumakov on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
